fedd7f38665b731eddf76300ec8866b10b1e433b
catperez81 <cat@healthsherpa.com>
2015-09-22 20:16:01 -0700

    update js

---
+++ js/application.js
@@ -0,0 +1,10 @@
+// $(document).ready(function() {
+// 	$( "#item" ).submit(function() {
+//  		 $var('li').append();
+// });
+
+// });
+
+$(document).ready(function() {
+  alert('Oh, hi there, world');
+});

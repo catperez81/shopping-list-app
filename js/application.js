4ea45caa5aa27cfa1e88cd46e7f59c9f02f415d0
catperez81 <cat@healthsherpa.com>
2015-09-23 19:05:20 -0700

    update jquery and html

--- js/application.js
+++ js/application.js
@@ -1,10 +1,24 @@
+
 // $(document).ready(function() {
-// 	$( "#item" ).submit(function() {
-//  		 $var('li').append();
+//   alert('Oh, hi there, world');
 // });
 
-// });
+
 
 $(document).ready(function() {
-  alert('Oh, hi there, world');
+// 	input item into field then show in list while clearing the field
+	$('#item').keypress(function(event){
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '13'){
+		// alert($('#item').val());
+			$('#list').append('<li class="item"><input type="checkbox"><label>'+$('#item').val()+'</label><img class="delete" src="images/delete.png"></li>');
+			$('#item').val('');
+		}
+	}); 
+
+	$('.delete').click(function(){
+	$(this).closest('li').remove();	
+	});
+
 });
+
